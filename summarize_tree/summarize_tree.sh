b4dd45fe2b11c360889023d21a2a8e93db3ca6b1
Armando Valdez <valde148@d.umn.edu>
2024-10-21 23:46:17 -0500

    changes

--- summarize_tree/summarize_tree.sh
+++ summarize_tree/summarize_tree.sh
@@ -6,15 +6,16 @@ if [ $# -ne 1 ]; then
 fi
 
 path=$1
-
+#will store the path argument if given
 if [ ! -d "$path" ]; then
     echo "Error: $path is not a valid."
     exit 1
 fi
+#if there is error it will exit out
 
 num_dirs=$(find "$path" -type d | wc -l)
-
+#will count num of directories and regular files with find & wc 
 num_regular=$(find "$path" -type f | wc -l)
-
+#prints any final counts
 echo "There were $num_dirs directories."
 echo "There were $num_regular regular files."
